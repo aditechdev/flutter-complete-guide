92dd09803e5c4c9d215c033244d86727632e4c31
aditechdev <aditechdevofficial@gmail.com>
2021-07-24 20:15:59 +0530

    :sparkles: Stateless widget should have final

--- lib/question.dart
+++ lib/question.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
   // const Questions({ Key? key }) : super(key: key);
 
-  String questionText;
+  final String questionText;
   Question(this.questionText);
 
   @override
