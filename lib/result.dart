c7370e95fe098c55fef360ffd9fbde9ea33a0c8d
aditechdev <aditechdevofficial@gmail.com>
2021-07-25 11:39:46 +0530

    :sparkles: Reset option added.

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler; 
+      //? We can use VoidCallBack instead of Function
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
@@ -25,13 +27,23 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text(
+              "Restart The Quiz!",
+            ),
+          ),
+        ],
       ),
     );
   }
